694259e0236ffbd9e8cc14b905a566e10005b93f
pablojr17 <pablojr17@gmail.com>
2021-05-31 19:09:07 -0300

    Create drawer

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => (
+  <NavigationContainer>
+    <Routes />
+  </NavigationContainer>
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/pages/MyLinks/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function MyLinks() {
+  return (
+    <View>
+      <Text>My links</Text>
+    </View>
+  )
+}
