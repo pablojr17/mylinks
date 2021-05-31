f37f0df7ff201607f332055dadd3e23035efc819
pablojr17 <pablojr17@gmail.com>
2021-05-31 20:55:41 -0300

    Linear - Gradient

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import { LinearGradient } from 'expo-linear-gradient';
 // import { Container } from './styles';
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <LinearGradient
+      // colors={['#005AA7', '#FFFDE4']}
+      // colors={['#0f0c29', '#302b63', '#24243e']}
+      colors={['#03001e', '#7303c0', '#fdeff9']}
+      style={{ flex: 1, justifyContent: 'center' }}
+    >
       <Text>Home</Text>
-    </View>
+    </LinearGradient>
   )
 }
